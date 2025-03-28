bbee367c5c7694239174914c48d4742bfe9b8c28
Beatriz Zinko <zinkobeatriz@gmail.com>
2025-03-27 21:01:47 -0300

    criado arquivo com tipos básicos de objetos no python

---
+++ tipos_de_objetos.py
@@ -0,0 +1,9 @@
+texto = "tipo texto" # str (string)
+numero_inteiro = 1 #int 
+booleano = True #bool (True ou False)
+numero_com_ponto_flutuante = 1.5 #float (numeros com virgula)
+lista = [1,2,3,4] #list
+dicionario = {#dict
+    "nome": "Beatriz",
+    "idade": 24
+}
