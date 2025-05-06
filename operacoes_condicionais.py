46f39ef32c0e02de7b043240c3f1a383b2330aab
Beatriz Zinko <zinkobeatriz@gmail.com>
2025-05-05 21:33:25 -0300

    estou aprendendo operações_condicionais por etapas

---
+++ operacoes_condicionais.py
@@ -0,0 +1,20 @@
+import random
+
+aluno_1_media_primeiro_trimestre = 7.2
+aluno_1_media_segundo_trimestre = 8.0
+aluno_1_media_terceiro_trimestre = 6.0
+
+aluno_2_media_primeiro_trimestre =2.5
+aluno_2_media_segundo_trimestre =3.5
+aluno_2_media_terceiro_trimestre =4.5
+
+aluno_3_media_primeiro_trimestre =1.5
+aluno_3_media_segundo_trimestre =8.5
+aluno_3_media_terceiro_trimestre =9.5
+
+aluno_4_media_primeiro_trimestre = round(random.uniform(0.0,10.0),2)
+aluno_4_media_segundo_trimestre = round(random.uniform(0.0,10.0),2)
+aluno_4_media_terceiro_trimestre = round(random.uniform(0.0,10.0),2)
+
+
+
